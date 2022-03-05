ad50fad47ca68a16eb6d77c1260c4eaff9bd7da5
Rachel Kaur <rachel.km@outlook.com>
2022-03-05 23:57:53 +0000

    add random number conditions for computer choice

--- rock_paper_scissors/app.js
+++ rock_paper_scissors/app.js
@@ -4,8 +4,10 @@ const userChoiceDisplay = document.getElementById('user-choice');
 const resultDisplay = document.getElementById('result');
 // Use query selector for all butttons
 const possibleChoices = document.querySelectorAll('button');
-// Initialise userChoice variable globally
+// Declare global userChoice variable
 let userChoice;
+// Declare global computerChoice variable
+let computerChoice;
  
 // Take the possible choices, and for each possible choice (button), add event listener to listen out for a click
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
@@ -21,5 +23,14 @@ possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click
 function generateComputerChoice() {
     // Generate a random whole number between 1-3
     const randomNumber = Math.floor(Math.random() * possibleChoices.length) + 1;
-    console.log(randomNumber);
+    // Test random number generator
+    // console.log(randomNumber);
+    if (randomNumber === 1) {
+        computerChoice = 'rock';
+    } else if (randomNumber === 2) {
+        computerChoice = 'paper';
+    } else {
+        computerChoice = 'scissors';
+    }
+    computerChoiceDisplay.innerHTML = computerChoice;
 }
