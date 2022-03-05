92065ee199930c5ed38165a4149a811605353d35
Rachel Kaur <rachel.km@outlook.com>
2022-03-05 20:47:44 +0000

    add button event listeners for user choice

---
+++ rock_paper_scissors/app.js
@@ -0,0 +1,16 @@
+// Select the span elements by ID
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const resultDisplay = document.getElementById('result');
+// Use query selector for all butttons
+const possibleChoices = document.querySelectorAll('button');
+// Initialise userChoice variable globally
+let userChoice;
+ 
+// Take the possible choices, and for each possible choice (button), add event listener to listen out for a click
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+   // Get the target id (i.e. whether id="rock", "paper", or "scissors") and store in userChoice variable
+   userChoice = e.target.id;
+   // Assign userChoice to userChoiceDisplay
+   userChoiceDisplay.innerHTML = userChoice;
+}))
